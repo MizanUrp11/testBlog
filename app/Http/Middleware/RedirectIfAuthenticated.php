2e70a6856851a23ea5e5f021f475cf9e8699e0b4
MizanUrp11 <mrn1105009@gmail.com>
2019-10-03 12:50:07 -0700

    Routing fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,9 +18,9 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check() && Auth::user()->role->id == 1) {
-            return redirect('admin.dashboard');
+            return redirect()->route('admin.dashboard');
         }elseif (Auth::guard($guard)->check() && Auth::user()->role->id == 2){
-            return redirect('author.dashboard');
+            return redirect()->route('author.dashboard');
         }else{
             return $next($request);
         }
