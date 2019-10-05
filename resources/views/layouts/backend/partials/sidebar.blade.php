1e7bb8dfa2d3649569d02194976b24568d7ccb41
MizanUrp11 <mrn1105009@gmail.com>
2019-10-04 19:19:38 -0700

    Admin and author dashboard setup

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -2,11 +2,11 @@
     <!-- User Info -->
     <div class="user-info">
         <div class="image">
-            <img src="images/user.png" width="48" height="48" alt="User" />
+            <img src="{{ asset('assets/backend/images/user.png') }}" width="48" height="48" alt="User" />
         </div>
         <div class="info-container">
-            <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
-            <div class="email">john.doe@example.com</div>
+            <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</div>
+            <div class="email">{{ Auth::user()->email }}</div>
             <div class="btn-group user-helper-dropdown">
                 <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                 <ul class="dropdown-menu pull-right">
@@ -32,12 +32,61 @@
     <div class="menu">
         <ul class="list">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active">
-                <a href="{{route('admin.dashboard')}}">
-                    <i class="material-icons">home</i>
-                    <span>Home</span>
-                </a>
-            </li>
+
+            @if(Request::is('admin*'))
+                <li class="{{ Request::is('admin/dashboard') ? 'active' : '' }}">
+                    <a href="{{route('admin.dashboard')}}">
+                        <i class="material-icons">dashboard</i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="{{ Request::is('admin/tag') ? 'active' : '' }}">
+                    <a href="{{route('admin.tag.index')}}">
+                        <i class="material-icons">labels</i>
+                        <span>Tags</span>
+                    </a>
+                </li>
+                <li class="header">System
+                <li>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        <i class="material-icons">input</i>
+                        <span>{{ __('Sign Out') }}</span>
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+                </li>
+            @endif
+            @if(Request::is('author*'))
+                <li class="{{ Request::is('author/dashboard') ? 'active' : '' }}">
+                    <a href="{{route('admin.dashboard')}}">
+                        <i class="material-icons">dashboard</i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="header">System
+                <li>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        <i class="material-icons">input</i>
+                        <span>{{ __('Sign Out') }}</span>
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+                </li>
+            @endif
+
+
+
+
         </ul>
     </div>
     <!-- #Menu -->
